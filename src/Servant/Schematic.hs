a9399466228c98c6a27c269738967ce4f448db64
Denis Redozubov <denis.redozubov@gmail.com>
2018-01-15 17:32:14 +0700

    WIP

--- src/Servant/Schematic.hs
+++ src/Servant/Schematic.hs
@@ -13,6 +13,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
@@ -27,7 +28,9 @@ import           Data.Proxy
 import           Data.Schematic
 import           Data.Schematic.JsonSchema
 import           Data.Singletons
+import           Data.Singletons.Prelude hiding ((:>))
 import           Data.Text
+import           Data.Type.Equality
 import           GHC.TypeLits
 import qualified JSONSchema.Draft4.Schema as D4
 import           Servant hiding (Tagged)
@@ -39,54 +42,100 @@ data Tagged (t :: k) a = Tagged a deriving (Functor, Show)
 instance ToJSON a => ToJSON (Tagged t a) where
   toJSON (Tagged a) = toJSON a
 
--- type family Expand (req :: Maybe q) (api :: k) :: l where
---   Expand 'Nothing api = api
---   Expand ('Just req) api = req :<|> api
+type family ConcatAlternative (l :: [k]) :: k where
+  ConcatAlternative (h ': tl) = ConcatAlternative' 'Nothing (h ': tl)
 
--- class HasSchema (req :: Maybe q) api where
---   type Schematized' req api :: k
+type family ConcatAlternative' (mapi :: Maybe s) (l :: [k]) :: k where
+  ConcatAlternative' ('Just api) '[] = api
+  ConcatAlternative' 'Nothing (h ': tl) = ConcatAlternative' ('Just h) tl
+  ConcatAlternative' ('Just api) (h ': tl) = ConcatAlternative' ('Just (api :<|> h)) tl
 
--- instance (HasSchema req a, HasSchema req b) => HasSchema req (a :<|> b) where
---   type Schematized' req (a :<|> b) = Schematized req a :<|> Schematized req b
+type family Expand (req :: Maybe q) (api :: [k]) :: [k] where
+  Expand 'Nothing api = api
+  Expand ('Just req) api = api :++ '[req]
 
--- instance {-# OVERLAPS #-} HasSchema req (Verb method code ctypes (JsonRepr schema)) where
---   type Schematized' req (Verb method code ctypes (JsonRepr schema)) =
---     Expand req
---       ("response" :> Get '[JSON] (Tagged schema D4.Schema) :<|>
---       Verb method code ctypes (JsonRepr schema))
+type family ConcatMaybe (path :: Maybe p) (pathSegment :: q) :: Maybe p where
+  ConcatMaybe 'Nothing q = q
+  ConcatMaybe ('Just a) q = 'Just (a :> q)
 
--- instance {-# OVERLAPPABLE #-} HasSchema req (Verb method code ctypes a) where
---   type Schematized' req (Verb method code ctypes a) =
---     Expand req (Verb method code ctypes a)
+type family PrefixMap (prefix :: k) (paths :: [p]) :: [p] where
+  PrefixMap p '[] = '[]
+  PrefixMap p (h ': tl) = p :> h ': PrefixMap p tl
 
--- instance {-# OVERLAPS #-} (HasSchema req sub) => HasSchema req (ReqBody cs (JsonRepr schema) :> sub) where
---   type Schematized' req (ReqBody cs (JsonRepr schema) :> sub) =
---     Schematized' ('Just ("request" :> Get '[JSON] (Tagged schema D4.Schema))) sub
+type family PrefixFirst (prefix :: k) (paths :: [p]) :: [p] where
+  PrefixFirst p (h ': tl) = p :> h ': tl
 
--- instance {-# OVERLAPPABLE #-} (HasSchema req sub) => HasSchema req (ReqBody cs a :> sub) where
---   type Schematized' req (ReqBody cs a :> sub) =
---     Schematized' 'Nothing sub
-
-type family Schematized' (req :: Maybe q) (api :: k) :: k where
-  Schematized' 'Nothing (Verb method code ctypes (JsonRepr schema)) =
-    "response" :> Get '[JSON] (Tagged schema D4.Schema) :<|>
-    Verb method code ctypes (JsonRepr schema)
-  Schematized' ('Just req) (Verb method code ctypes (JsonRepr schema)) =
-    req :<|>
-    "response" :> Get '[JSON] (Tagged schema D4.Schema) :<|>
-    Verb method code ctypes (JsonRepr schema)
-  Schematized' 'Nothing (Verb method code ctypes a) = Verb method code ctypes a
-  Schematized' ('Just req) (Verb method code ctypes a) =
-    req :<|> Verb method code ctypes a
-  Schematized' req (ReqBody cs (JsonRepr schema) :> sub) =
-    Schematized'
-      ('Just ("request" :> Get '[JSON] (Tagged schema D4.Schema))) sub
-  Schematized' req (ReqBody cs a :> sub) = ReqBody cs a :> Schematized' req sub
-  Schematized' req (a :<|> b) = Schematized' req a :<|> Schematized' req b
-  Schematized' req (a :> sub) = a :> Schematized' req sub
+type family Schematized' (path :: Maybe p) (req :: Maybe q) (api :: k) :: [k] where
+  Schematized' mpath mreq (Verb method code ctypes (JsonRepr schema)) =
+    Expand mreq
+      '[ Verb method code ctypes (JsonRepr schema)
+      , "response" :> Get '[JSON] (Tagged schema D4.Schema) ]
+  Schematized' mpath mreq (Verb method code ctypes a) =
+    Expand mreq '[Verb method code ctypes a]
+  Schematized' mpath mreq (ReqBody cs (JsonRepr schema) :> sub) =
+    PrefixFirst (ReqBody cs (JsonRepr schema))
+      (Schematized'
+        mpath
+        ('Just ("request" :> Get '[JSON] (Tagged schema D4.Schema)))
+        sub)
+  Schematized' mpath mreq (ReqBody cs a :> sub) =
+    PrefixFirst (ReqBody cs a) (ConcatAlternative (Schematized' mpath mreq sub))
+  Schematized' mpath mreq (a :<|> b) = Schematized' mpath mreq a :++ Schematized' mpath mreq b
+  Schematized' mpath mreq (a :> sub) = (PrefixMap a (Schematized' (ConcatMaybe mpath a) mreq sub))
 
 type family Schematized (api :: k) :: k where
-  Schematized api = Schematized' 'Nothing api
+  Schematized api = ConcatAlternative (Schematized' 'Nothing 'Nothing api)
+
+-- Typechecking tests
+
+type TestApiSchematic1 =
+  "api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull)
+
+_test1 :: (Schematized TestApiSchematic1) :~~:
+  ("api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull)
+  :<|> "api" :> "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema)
+  :<|> "api" :> "request"  :> Get '[JSON] (Tagged 'SchemaNull D4.Schema))
+_test1 = HRefl
+
+-- Schematic TestApiSchematic
+-- ConcatAlternative (PrefixMap "api" (Schematized' ('Just "api") 'Nothing ("api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull))))
+-- ConcatAlternative' 'Nothing (PrefixMap "api" (Schematized' ('Just "api") 'Nothing ("api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull))))
+-- ConcatAlternative' 'Nothing (PrefixMap "api" (Schematized' ('Just "api") 'Nothing (ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull))))
+-- ConcatAlternative' 'Nothing (PrefixMap "api" (PrefixFirst (ReqBody '[JSON] (JsonRepr 'SchemaNull)) (Schematized' ('Just "api") ('Just ("request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema))) (Post '[JSON] (JsonRepr 'SchemaNull)))))
+-- ConcatAlternative' 'Nothing (PrefixMap "api" (PrefixFirst (ReqBody '[JSON] (JsonRepr 'SchemaNull)) (Expand ('Just ("request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema))) '[Post '[JSON] (JsonRepr 'SchemaNull), "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema)])))
+-- ConcatAlternative' 'Nothing (PrefixMap "api" (PrefixFirst (ReqBody '[JSON] (JsonRepr 'SchemaNull)) '[Post '[JSON] (JsonRepr 'SchemaNull), "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema)] :++ ["request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema]))
+-- ConcatAlternative' 'Nothing (PrefixMap "api" (PrefixFirst (ReqBody '[JSON] (JsonRepr 'SchemaNull)) '[Post '[JSON] (JsonRepr 'SchemaNull), "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema), "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema]))
+-- ConcatAlternative' 'Nothing (PrefixMap "api" ('[ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull), "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema), "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema]))
+-- ConcatAlternative' 'Nothing ("api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull) ': PrefixMap "api" '["response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema), "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema]))
+-- ConcatAlternative' 'Nothing ("api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull) ': "api" :> "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema) ': PrefixMap "api" '["request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema)])
+-- ConcatAlternative' 'Nothing ("api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull) ': "api" :> "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema) ': "api" :> "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema) ': PrefixMap "api" '[])
+-- ConcatAlternative' 'Nothing ("api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull) ': "api" :> "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema) ': "api" :> "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema) ': '[])
+-- ConcatAlternative' 'Nothing '["api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull), "api" :> "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema), "api" :> "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema)])
+-- ConcatAlternative' ('Just ("api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull))) '["api" :> "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema), "api" :> "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema)])
+-- ConcatAlternative' ('Just ("api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull) :<|> "api" :> "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema))) '["api" :> "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema)])
+-- ConcatAlternative' ('Just ("api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull) :<|> "api" :> "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema) :<|> "api" :> "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema))) '[])
+-- "api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] (JsonRepr 'SchemaNull) :<|> "api" :> "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema) :<|> "api" :> "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema)
+
+type TestApiSchematic2 =
+  "api" :> Post '[JSON] (JsonRepr 'SchemaNull)
+
+_test2 :: Schematized TestApiSchematic2 :~:
+  "api" :> Post '[JSON] (JsonRepr 'SchemaNull)
+  :<|> "api" :> "response" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema)
+_test2 = Refl
+
+type TestApiSchematic3 = "api" :> Post '[JSON] ()
+
+_test3 :: Schematized TestApiSchematic3 :~: TestApiSchematic3
+_test3 = Refl
+
+type TestApiSchematic4 =
+  "api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] ()
+
+_test4 :: Schematized TestApiSchematic4 :~:
+  "api" :> ReqBody '[JSON] (JsonRepr 'SchemaNull) :> Post '[JSON] () :<|>
+  "api" :> "request" :> Get '[JSON] (Tagged 'SchemaNull D4.Schema)
+_test4 = Refl
 
 -- | An implementation for json-schema introspection handlers generated by
 -- @Schematized@ type family. It works with requests and response schemas.
@@ -94,4 +143,4 @@ schemaHandler
   :: forall m tag (schema :: Schema) a
    . (SingI schema, Monad m)
   => m (Tagged schema D4.Schema)
-schemaHandler = pure . Tagged . fromJust $ toJsonSchema (Proxy @schema)
+schemaHandler  = pure . Tagged . fromJust $ toJsonSchema (Proxy @schema)
